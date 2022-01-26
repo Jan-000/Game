dac46dcfafd7a6867f7dfd00a5cc1b2776a90da1
Jan Gorecki <jasiekgorecki@gmail.com>
2022-01-26 14:41:37 +0100

    only update of progress

--- game.js
+++ game.js
@@ -1,60 +1,107 @@
-var example = document.getElementById("example");
-var context = example.getContext("2d");
-context.fillStyle = "rgb(255,166,0)";
-context.fillRect(100, 30, 200, 200);
+let targetR
+let targetG
+let targetB
 
-function gradientRect(){
-var grd = context.createLinearGradient(90, 0, 670, 0);
-grd.addColorStop(0.05, "magenta");
-grd.addColorStop(0.25, "blue");
-grd.addColorStop(0.5, "green");
-grd.addColorStop(0.6, "yellow");
-grd.addColorStop(0.7, "red")
-grd.addColorStop(0.95, "white");
-context.fillStyle = grd;
-context.fillRect(100, 580, 700, 200)}
+function genTargetColor(){
 
-gradientRect()
+  targetR = Math.floor(Math.random()*255)
+  targetG = Math.floor(Math.random()*255)
+  targetB = Math.floor(Math.random()*255)
+}
 
+genTargetColor()
 
+let example = document.getElementById("example");
+let context = example.getContext("2d");
+context.fillStyle = `rgb(${targetR},${targetG},${targetB})`;
+context.fillRect(100, 30, 150, 150);
 
-let statusOfClick = false;
+ function genColorChart(){
+ let grd = context.createLinearGradient(90, 0, 900, 0);
+  grd.addColorStop(0.05, "magenta");
+  grd.addColorStop(0.25, "blue");
+  grd.addColorStop(/*((Math.random()).toFixed(2))*/0.5, "green");
+  grd.addColorStop(0.6, "yellow");
+  grd.addColorStop(0.7, "red")
+  grd.addColorStop(0.95, "white");
+  context.fillStyle = grd;
+  context.fillRect(100, 390, 900, 150)
+}
+
+//call function
+  genColorChart()
+
+  let colorAccuracy
+  let statusOfClick = true;
+  //let countOfClicks = 0
+
 
 function toggleStatusOfClick() {
   statusOfClick = !statusOfClick;
   console.log("current status of statusOfClick: ", statusOfClick);
+  console.log("current color is", currentColor)
+
+  if (!statusOfClick){
+  calcAccuracy()
+  listResults(colorAccuracy)}
+  //document.body.style.setProperty('--main-color',"#00aa00")
+  //countOfClicks ++
+  //console.log("count of clicks is: ", countOfClicks)
 }
 
+
+
+function calcAccuracy(){
+  //console.log("target B is: ",targetB)
+  //console.log("user B is: ", userB)
+  colorAccuracy = ((1-((((targetR - userR)**2 + (targetG - userG)**2 + (targetB - userB)**2)**0.5)/(255**2+255**2+255**2)**0.5))*100).toFixed(0)
+  console.log("typeof colorAccuracy is", typeof(colorAccuracy))
+  console.log("accuracy equals", colorAccuracy)
+
+}
+
+
+listResults = function(arg) {
+
+  let results = document.getElementById("results");
+      var newLi = document.createElement("li");
+      newLi.innerHTML = `${arg}%`;
+      results.appendChild(newLi);
+}
+
+
 let clickableSpace = document.getElementById("example");
-clickableSpace.addEventListener("click", toggleStatusOfClick);
+    clickableSpace.addEventListener("click", toggleStatusOfClick);
+
+    let currColor
+    let userR, userG, userB
 
 function hoverRgb(e) {
+
   if (statusOfClick == true) {
     var x = e.pageX;
     var y = e.pageY;
     var c = document.getElementById("example").getContext("2d");
     var p = c.getImageData(x, y, 1, 1).data;
-
     context.fillStyle = `rgb(${p[0]},${p[1]},${p[2]})`;
-    context.fillRect(100, 300, 200, 200);
+    context.fillRect(100, 210, 150, 150);
 
     $("#status").html(
       `${p[0]}` + `\xa0\xa0` + `${p[1]}` + `\xa0\xa0` + `${p[2]}`
     );
-  }
+     userR = p[0]
+     userG = p[1]
+     userB = p[2]
+
+  } currentColor = [userR, userG, userB]
+
 }
 
+//abhören
 document.getElementById("example").addEventListener("mousemove", hoverRgb);
 
-// console.log("last message", statusOfClick)
 
-/*    $('#example').click(function(e) {
+//flag 
 
-        var x = e.pageX;
-        var y = e.pageY;
-        var coord2 = "position x=" + x + ", y=" + y;
-        var c2 = this.getContext("2d");
-        var p2 = c2.getImageData(x, y, 1, 1).data;
-        console.log(p2)
-    }) */
+
 
